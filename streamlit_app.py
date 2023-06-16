3d86e0cb997b6d6db8f64ac104c1bc65eadbce32
Axel G313248317-51 <galamaoude@icloud.com>
2023-06-16 18:09:31 +0200

    test header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ streamlit.title('Bienvenue au PMU')
 streamlit.text('Prenez vos paris')
 
 #streamlit.header('🐎 cote de Apollo Quick à 9 contre 1')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text('Alimentation du cheval')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
