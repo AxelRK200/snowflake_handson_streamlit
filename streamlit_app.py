598e6026211fdc17fd7bf2bf2f4cc9fd0b89e55f
Axel G313248317-51 <galamaoude@icloud.com>
2023-06-16 18:10:22 +0200

    restore_header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,8 @@ import pandas as pd
 streamlit.title('Bienvenue au PMU')
 streamlit.text('Prenez vos paris')
 
-#streamlit.header('🐎 cote de Apollo Quick à 9 contre 1')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.header('🐎 cote de Apollo Quick à 9 contre 1')
 streamlit.text('Alimentation du cheval')
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
