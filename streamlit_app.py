5532c0df7f6c3a10c5cb7656068d3d423f8b3d6f
Axel G313248317-51 <galamaoude@icloud.com>
2023-06-16 17:57:50 +0200

    ajout dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
+import pandas
 
 streamlit.title('Bienvenue au PMU')
 streamlit.text('Prenez vos paris')
 
 streamlit.header('🐎 cote de Apollo Quick à 9 contre 1')
+streamlit.text('Alimentation du cheval')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
