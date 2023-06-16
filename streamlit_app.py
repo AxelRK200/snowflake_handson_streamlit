62cd5b4e709279bb53c054d0f7ec1af5621f2841
Axel G313248317-51 <galamaoude@icloud.com>
2023-06-16 18:05:14 +0200

    fix df ?

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit
-import pandas
+import pandas as pd
 
 streamlit.title('Bienvenue au PMU')
 streamlit.text('Prenez vos paris')
 
 streamlit.header('🐎 cote de Apollo Quick à 9 contre 1')
+
 streamlit.text('Alimentation du cheval')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
