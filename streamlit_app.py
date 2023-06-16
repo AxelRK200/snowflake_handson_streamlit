b8df6a57bdd10adb509bbb6c1d12f38e7fccbe43
Axel G313248317-51 <galamaoude@icloud.com>
2023-06-16 18:07:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 streamlit.title('Bienvenue au PMU')
 streamlit.text('Prenez vos paris')
 
-streamlit.header('🐎 cote de Apollo Quick à 9 contre 1')
+#streamlit.header('🐎 cote de Apollo Quick à 9 contre 1')
 
 streamlit.text('Alimentation du cheval')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
